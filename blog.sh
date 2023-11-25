95f7793e9882160edbadcbce355a853daba424b3
seunhub007 <sadiboseun@yahoo.com>
2023-11-25 23:31:24 +0100

    updated5

--- blog.sh
+++ blog.sh
@@ -15,11 +15,11 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 
 mkdir actions-runner && cd actions-runner
-curl -o actions-runner-osx-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-osx-x64-2.311.0.tar.gz
-echo "f4d8d1dd850fd0889e0d250c82fa587b0e21934f8441143ee6772284b2ae6211  actions-runner-osx-x64-2.311.0.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-osx-x64-2.311.0.tar.gz
-echo -ne "\n" | ./config.sh --url https://github.com/seunhub007/Bitblog --token A4ZVN33YRSZSK6G5JW553ITFMJ4KA
+curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
+echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
+echo -ne "\n" | ./config.sh --url https://github.com/seunhub007/Bitblog --token A4ZVN37V2PSQBNSDXDBKTPTFMKAVM
 ./run.sh&
